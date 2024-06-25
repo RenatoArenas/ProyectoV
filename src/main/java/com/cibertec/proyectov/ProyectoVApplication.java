65a2329bf243e4cd741624cbdb22142375cf8c39
Renato Arenas <renato.arenas2012@gmail.com>
2024-06-25 18:18:35 -0500

    Primer commit

---
+++ src/main/java/com/cibertec/proyectov/ProyectoVApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.proyectov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoVApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/proyectov/ProyectoVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.proyectov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
